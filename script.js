d1b6014dcf1982a59c3fe5f7b71657585d91f3df
shalva-davitashvili <shalva.davitaashvili1@gmail.com>
2021-07-09 17:38:56 +0400

    finish

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let x = 20; 
+let x = 19.19; 
+let x = "hello"; 
+let x = "Script"; 
+let x = true;  
+let x = false; 
+let x = null;
+let x = undefined;
+
+
+let x = 9;
+let y = 3.14*x*y;
+console.log('x',x)
+let z = 2*3.14*x*x;
+console.log('z',z)
+
+
+let x = 12;
+let y = 35;
+let z = Math.sqrt(x*x + y*y)
+
+
+
+let Buckwheat = prompt('write Buckwheat')
+let x = Buckwheat*Buckwheat*Buckwheat;
+console.log('cube Buckwheat',x)
+
